7a9ae2404c2e219fc8b71c4a2ba91ec46c9ba0b0
meganm672 <meganm672@gmail.com>
2024-04-04 12:02:09 -0400

    remove code

--- src/components/sections/reservePages/BookingForm.jsx
+++ src/components/sections/reservePages/BookingForm.jsx
@@ -59,17 +59,6 @@ export default function ReservationForm(props) {
     setLName(e.target.value)
   }
 
-  function handleEmailChange(e){
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    if ( re.test(e) ) {
-      setEmail(e.target.value)
-    }
-    else {
-        setError("Please enter a valid email")
-    }
-
-  }
 
   return (
     <form className="reservation-form">
